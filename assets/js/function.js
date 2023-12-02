02113a9d12b6eadfe1b9c81461f3fd51c4eb8fc0
danielcontreras205 <danielcontreras205@gmail.com>
2023-12-01 22:57:25 -0500

    Ajuste Modales

--- assets/js/function.js
+++ assets/js/function.js
@@ -11,9 +11,17 @@ function modal(x){
       phat = "assets/modales/modal3.html";
     }
     document.getElementById("iframer").src=phat;
+
+    // Agregar un listener para el evento de tecla "Esc"
+    window.addEventListener('keydown', function(event) {
+      if (event.key === 'Escape') {
+        cerrarModal(modal);
+      }
+    });
 }
 //cerrar modal
 const botonmodal = document.getElementById('cerrarModal');
+
 botonmodal.addEventListener('click', function (){
   const modal = document.getElementById('modal1');
   modal.classList.replace('modalPublico','modalOculto');
@@ -21,3 +29,17 @@ botonmodal.addEventListener('click', function (){
   var phat = "assets/modales/Error.html";
   document.getElementById("iframer").src=phat;
 });
+
+
+function cerrarModal(modal) {
+  modal.classList.replace('modalPublico','modalOculto');
+  // redirecionar el modal al de rror para cortar video de la modal 2 
+  var phat = "assets/modales/Error.html";
+  document.getElementById("iframer").src=phat;
+  // Remover el listener cuando se cierra la modal
+  window.removeEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      cerrarModal();
+    }
+  });
+}
