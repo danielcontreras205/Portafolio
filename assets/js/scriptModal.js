9f2f5c1e02e2e1456226f7d013b30cc9323365d0
danielcontreras205 <danielcontreras205@gmail.com>
2023-12-04 02:52:54 -0500

    Update

--- assets/js/scriptModal.js
+++ assets/js/scriptModal.js
@@ -23,3 +23,26 @@ var swiper = new Swiper(".swiper", {
       clickable: true
     }
   });
+  //---------------------------------- Definir la consulta de medios -----------------------------------------
+var mediaQuery = window.matchMedia("(max-width: 720px)");
+
+// Función que se ejecutará cuando cambie el estado de la consulta de medios
+function handleMediaQueryChange(mediaQuery) {
+  var pageWidth = window.innerWidth;
+  var pageHeight = window.innerHeight;
+  if (mediaQuery.matches) {
+    // El ancho de la pantalla es máximo 720px
+    console.log("La pantalla tiene un ancho máximo de 720px o menos.");
+    console.log("Ancho de la página:", pageWidth, "Alto de la página:", pageHeight);
+  } else {
+    // El ancho de la pantalla es mayor a 720px
+    console.log("La pantalla tiene un ancho mayor a 720px.");
+    console.log("Ancho de la página:", pageWidth, "Alto de la página:", pageHeight);
+  }
+}
+
+// Ejecutar la función al cargar la página
+handleMediaQueryChange(mediaQuery);
+
+// Añadir un "escucha" para cambios en la consulta de medios
+mediaQuery.addEventListener('change',handleMediaQueryChange);
