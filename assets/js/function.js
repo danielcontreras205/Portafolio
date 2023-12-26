5cf0b2e1b3e1c1f748a295b9901c8c29f8460b2b
danielcontreras205 <danielcontreras205@gmail.com>
2023-12-25 20:02:15 -0500

    Update Orden

--- assets/js/function.js
+++ assets/js/function.js
@@ -103,6 +103,87 @@ function LecturaDeVisitas() {
     });
 }
 
+//----------------------------- contador de pagina localStorage ----------------------------------------
+function CrearlocalStorage (contadorLocalStorage){
+  if (window.localStorage) {
+    // localStorage está habilitado
+    console.log("localStorage está habilitado.");
+    // Verificamos si ya existe un contador en el almacenamiento local
+    if (localStorage.getItem("visitCount")) {
+      // Si existe, incrementamos el contador
+      let count = contadorLocalStorage;
+      localStorage.setItem("visitCount", count);
+      document.getElementById("count").textContent = count;
+    } else {
+      // Si no existe, inicializamos el contador a 1
+      localStorage.setItem("visitCount", contadorLocalStorage);
+      document.getElementById("count").textContent = contadorLocalStorage;
+    }
+  } else {
+    // localStorage no está habilitado
+    console.log("localStorage no está habilitado. No podrás utilizarlo.");
+  }
+}
+
+//----------------------------- contador de pagina cookie----------------------------------------
+function CrearCooking (contadorCooking){
+  if (navigator.cookieEnabled === false) {
+    alert("Por favor, habilita las cookies en tu navegador para usar todas las funciones del sitio.");
+  } else {
+    setCookie("miCookie", contadorCooking, 7); // La cookie expirará en 7 dias
+  }
+}
+function setCookie(nombre, valor, expiracionDias) {
+  if (cookieExiste(nombre)) {
+    console.log('existe la cookie');
+    const cookies = document.cookie.split(';');
+    // Buscar la cookie específica por nombre
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        console.log(cookie);
+        // Verificar si la cookie tiene el nombre que estamos buscando
+        /*  
+            el método startsWith() se utiliza para determinar si una cadena 
+            comienza con los caracteres de otra cadena. Este método devuelve 
+            true si la cadena invocadora comienza con la cadena especificada 
+            como argumento
+        */
+        if (cookie.startsWith(nombre + '=')) { 
+            // Obtener y devolver el valor de la cookie, la base es 10, que es decimal.
+            // valor = parseInt(cookie.substring(nombre.length + 1), 10) + 1;
+        }
+    }
+    updateCoockie(nombre,valor,expiracionDias);
+  } else {
+    console.log('No Existe la cookie');
+    var fechaExpiracion = new Date();
+    fechaExpiracion.setDate(fechaExpiracion.getDate() + expiracionDias);
+    var cookie = nombre + "=" + valor + ";expires=" + fechaExpiracion.toUTCString() + ";path=/";
+    document.cookie = cookie;
+  }
+}
+// Función para verificar si una cookie existe
+function cookieExiste(nombreCookie) {
+  var cookies = document.cookie.split(';');
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    // Verificar si la cookie comienza con el nombre proporcionado
+    if (cookie.indexOf(nombreCookie + '=') === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+// actualiza coockie
+function updateCoockie(nombre, valor, expiracionDias) {
+  console.log('se actualiza coockie');
+  var fechaExpiracion = new Date();
+  fechaExpiracion.setDate(fechaExpiracion.getDate() + expiracionDias);
+  var cookie = nombre + "=" + valor + ";expires=" + fechaExpiracion.toUTCString() + ";path=/";
+  document.cookie = cookie;
+  document.getElementById("countCookies").textContent = valor;
+}
+
 
 
 
